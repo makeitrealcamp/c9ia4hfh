93b705e73bda59d3e68bf69f088886063a321559
camilo9020 <camilo9020@gmail.com>
2015-08-24 10:05:48 -0500

    Se agrega la funcionalidad de editar y eliminar habitaciones

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -15,6 +15,32 @@ class RoomsController < ApplicationController
       render :new
     end
   end
+    
+
+  def edit
+
+    @room= Room.find(params[:id])
+
+
+  end
+
+  def update
+    @room= Room.find(params[:id])
+
+    if @room.update(room_params)
+      redirect_to rooms_path, notice: 'La Habitación ha sido editada con exitó'
+    else
+      render  :edit
+    end
+  end
+
+  def destroy
+    @room= Room.find(params[:id])
+    @room.destroy
+    redirect_to rooms_path, notice: 'La Habitación ha sido eliminada con exitó'
+  end
+
+
 
   protected
     def room_params
--- app/models/room.rb
+++ app/models/room.rb
@@ -11,9 +11,7 @@
 #  updated_at  :datetime         not null
 #  image_url   :string
 #
-
 class Room < ActiveRecord::Base
-
 	validates :title, :description, :beds, :guests, :image_url, :price_per_night, presence: true
 	validates :description, length: {maximum: 400}
 	validates :beds, numericality: { only_integer: true}
