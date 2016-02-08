37c0ea54d5b70b6a5410498b4e43a9d8a723d0a6
pandai87 <pandai_87@hotmail.com>
2016-02-08 09:34:30 -0500

    Se agrea validacion de datos con sus mensajes de error

--- app/models/room.rb
+++ app/models/room.rb
@@ -13,4 +13,26 @@
 #
 
 class Room < ActiveRecord::Base
+
+
+validates :title,
+			presence: { message: "falta el titulo" }
+
+
+validates :description, 
+		  presence: { message: "falta la descripcion" },
+		  length: {maximum: 400, message: "la descripción es muy larga"}
+		   
+
+
+validates :beds, presence: true, numericality: { only_integer: true, message: "bed no es un numero"}
+			#message: ''
+
+
+validates :guests, presence: true, numericality: { only_integer: true, message: "guests no es un numero" }
+			#message: ''
+
+validates :image_url, presence: true
+			#message: ''
+
 end
