065e005d2d175d1d26efc3d914a8c5ac83b9e3c2
hermesagm <hermesagm@gmail.com>
2016-08-23 11:41:27 -0500

    Se realizan rutas edit  y delete de Rooms

--- app/models/room.rb
+++ app/models/room.rb
@@ -13,9 +13,4 @@
 #
 
 class Room < ActiveRecord::Base
-  validates :title, presence: true
-  validates :description, presence:true , length: { maximum:400}
-  validates :beds, presence:true, numericality: {only_integer:true}
-  validates :guests, presence:true, numericality: {only_integer:true}
-  validates :image_url, presence:true
 end
