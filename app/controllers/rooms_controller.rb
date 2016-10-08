211ced530513cbface3459fa9cee52efe185bfac
ja7rodal <ja7rodal@gmail.com>
2016-10-08 18:48:24 -0500

    Action edit and delet room

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -16,8 +16,25 @@ class RoomsController < ApplicationController
     end
   end
 
+  def edit
+    @room = Room.find(params[:id])
+  end
+
+  def update
+    @room =  Room.find(params[:id])
+    @room.update(room_params)
+    redirect_to rooms_path
+  end
+
+  def destroy
+    @room = Room.find(params[:id])
+    @room.destroy
+    redirect_to rooms_path
+  end
+
+
   protected
-    def room_params
-      params.require(:room).permit(:title, :description, :beds, :guests, :price_per_night, :image_url)
-    end
+  def room_params
+    params.require(:room).permit(:title, :description, :beds, :guests, :price_per_night, :image_url)
+  end
 end
