e63544fadd3ce65ff5baecbc4d1f5c26c5b34b3b
Jara2392 <daniel.jaramillog@udea.edu.co>
2017-02-17 11:56:29 -0500

    Edit action already implemented

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170217141739) do
+ActiveRecord::Schema.define(version: 20170217143504) do
 
   create_table "rooms", force: :cascade do |t|
     t.string   "title"
@@ -21,7 +21,7 @@ ActiveRecord::Schema.define(version: 20170217141739) do
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.string   "image_url"
-    t.decimal  "price_per_nigth"
+    t.decimal  "price_per_night"
   end
 
 end
