e2c64abbbc250544cdaf2e15f04411aee0fcdcf6
danielmoralesp <daniel_moralesp@hotmail.com>
2017-02-21 23:11:13 -0500

    Ejercicio resuelto

--- Gemfile
+++ Gemfile
@@ -22,6 +22,8 @@ gem 'turbolinks'
 gem 'jbuilder', '~> 2.0'
   # bundle exec rake doc:rails generates the API under doc/api.     # bundle exec rake doc:rails generates the API under doc/api.
 gem 'sdoc', '~> 0.4.0', group: :doc
+#Gema pra los errores
+gem 'annotate'
 
 
 group :development do
--- app/models/room.rb
+++ app/models/room.rb
@@ -13,4 +13,8 @@
 #
 
 class Room < ActiveRecord::Base
+  validates :title, :image_url, presence: true
+  validates :description, presence: true, length: { maximum: 400 }
+  validates :beds, presence: true, numericality: { only_integer: true }
+  validates :guests, presence: true, numericality: { only_integer: true }
 end
