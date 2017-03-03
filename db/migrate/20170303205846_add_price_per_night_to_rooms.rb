7a4455775384f6188012818bc21e8cc03b4d6541
Daniel Tobon <shiriux1@gmail.com>
2017-03-03 16:20:08 -0500

    ahora si

--- db/migrate/20170303205846_add_price_per_night_to_rooms.rb
+++
@@ -1,5 +0,0 @@
-class AddPricePerNightToRooms < ActiveRecord::Migration
-  def change
-    add_column :rooms, :Price_per_night, :decimal
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170303205846) do
+ActiveRecord::Schema.define(version: 20170303211754) do
 
   create_table "rooms", force: :cascade do |t|
     t.string   "title"
@@ -21,7 +21,7 @@ ActiveRecord::Schema.define(version: 20170303205846) do
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.string   "image_url"
-    t.decimal  "Price_per_night"
+    t.decimal  "price_per_night"
   end
 
 end
