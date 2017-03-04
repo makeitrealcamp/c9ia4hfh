f308b8e3030ff01f2e617e8b9b530066cd2b98e2
Daniel Tobon <shiriux1@gmail.com>
2017-03-04 18:37:14 -0500

    reto siete

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -16,6 +16,36 @@ class RoomsController < ApplicationController
     end
   end
 
+
+  def edit
+
+    @room = Room.find(params[:id])
+    
+  end
+
+
+  def update
+   @room = Room.find(params[:id])
+    @room.update(room_params)
+
+     if @room.save
+      redirect_to rooms_path
+    else
+      render :edit
+    end
+
+    
+  end
+
+
+  def destroy
+
+     @room = Room.find(params[:id])
+     @room.destroy
+     redirect_to rooms_path
+    
+  end
+
   protected
     def room_params
       params.require(:room).permit(:title, :description, :beds, :guests, :image_url,:price_per_night)
