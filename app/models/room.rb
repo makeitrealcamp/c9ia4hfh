3ee08fce8274ef6a3a16a43cc6d6e4e0660ad27d
Adriana Agudelo-Toro <adriana.agudelo@nrg-edge.com>
2017-03-12 18:09:50 -0500

    Adding only decimals

--- app/models/room.rb
+++ app/models/room.rb
@@ -17,5 +17,5 @@ class Room < ActiveRecord::Base
   validates :description, length: { maximum: 400 }
   validates :beds, numericality: { only_integer: true }
   validates :guests, numericality: { only_integer: true }
-  validates :price_per_night, numericality: { only_integer: true }
+  validates :price_per_night, numericality: { only_decimal: true }
 end
