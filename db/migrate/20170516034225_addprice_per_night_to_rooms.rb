564e1cc75dddc8c84d66659ad94afc481e1d5b2d
kldgarrido <kldgarrido@gmail.com>
2017-05-15 23:01:20 -0500

    add new column 2

--- db/migrate/20170516034225_addprice_per_night_to_rooms.rb
+++
@@ -1,5 +0,0 @@
-class AddpricePerNightToRooms < ActiveRecord::Migration[5.0]
-  def change
-  	add_column price_per_night:decimal
-  end
-end
--- db/migrate/20170516034616_add2price_per_night_to_rooms.rb
+++
@@ -1,4 +0,0 @@
-class Add2pricePerNightToRooms < ActiveRecord::Migration[5.0]
-  def change
-  end
-end
