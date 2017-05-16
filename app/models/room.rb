98bf42ac91a01312a78834d4ed30968922b52902
kldgarrido <kldgarrido@gmail.com>
2017-05-15 22:20:19 -0500

    apply validation

--- app/models/room.rb
+++ app/models/room.rb
@@ -13,4 +13,11 @@
 #
 
 class Room < ActiveRecord::Base
+	validates :title, presence: true
+	validates :description, presence: true, length: { maximum: 400}
+	validates :beds, presence: true, numericality: {:only_integer => true}
+	validates :guests,  presence: true, numericality: {:only_integer => true}
+	#validates :created_at,  presence: true
+	#validates :updated_at,  presence: true
+	validates :image_url,  presence: true
 end
