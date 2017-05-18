4c13535105e7c434f76121c5615657f73c8773cb
kldgarrido <kldgarrido@gmail.com>
2017-05-17 19:49:56 -0500

    edit feature

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -7,17 +7,24 @@ class RoomsController < ApplicationController
     @room = Room.new
   end
 
+  def edit
+    @room = Room.find(params[:id])
+  end
+
   def create
     @room = Room.new(room_params)
     if @room.save
       redirect_to rooms_path
     else
+      @errors = @room.errors.full_messages
       render :new
     end
   end
 
+
+
   protected
     def room_params
-      params.require(:room).permit(:title, :description, :beds, :guests, :image_url)
+      params.require(:room).permit(:title, :description, :beds, :guests, :image_url, :price_per_night)
     end
 end
