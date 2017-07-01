f6d4e5f8df98f0adb6171cb9efa493623edc8e60
efectovirtual <albertorodriguez@MacBook-Pro-de-Alberto.local>
2017-07-01 00:20:14 -0500

    Precio por noche

--- db/schema.rb
+++ db/schema.rb
@@ -17,10 +17,10 @@ ActiveRecord::Schema.define(version: 20170701050407) do
     t.text     "description"
     t.integer  "beds"
     t.integer  "guests"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.datetime "created_at",                              null: false
+    t.datetime "updated_at",                              null: false
     t.string   "image_url"
-    t.decimal  "price_per_night"
+    t.decimal  "price_per_night", precision: 6, scale: 2
   end
 
 end
