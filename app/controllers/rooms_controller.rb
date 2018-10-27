cecb6e356e40018e72c5ec3ff2ba0467537c49b6
Hugo Tamayo <doctamayot@hotmail.com>
2018-10-26 20:41:51 -0500

    implementa edit

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -16,6 +16,24 @@ class RoomsController < ApplicationController
     end
   end
 
+  def edit
+    @room = Room.find(params[:id])
+  end
+
+  
+  
+
+  end
+  def update 
+    @room = Room.find(params[:id])
+    if @room.update(room_params)
+        redirect_to rooms_path, notice: "El room ha sido modificado"
+    else
+        render :edit
+    end    
+end
+
+
   protected
     def room_params
       params.require(:room).permit(:title, :description, :beds, :guests, :image_url)
