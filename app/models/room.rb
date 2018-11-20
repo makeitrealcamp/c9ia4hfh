4a784508664dbcb480b538338f494c48a00cd517
lauracb <lauracadavidb@gmail.com>
2018-11-20 13:55:57 -0500

    Se añadieron las validaciones en el modelo y la vista de la nueva columna

--- app/models/room.rb
+++ app/models/room.rb
@@ -13,9 +13,10 @@
 #
 
 class Room < ActiveRecord::Base
-    validates :title, :description, :beds, :guests, :image_url, presence: true
+    validates :title, :description, :beds, :guests, :image_url, :price_per_night, presence: true
     validates :description, length: {maximum: 400}
     validates :beds, numericality: {only_integer: true}
     validates :guests, numericality: {only_integer: true}
+    validates :price_per_night, numericality: true 
 end
 
