fbd94c6de261a98e93560b07c29d36a326fa8511
Eric Suescun <edsuescun@gmail.com>
2019-03-12 10:12:06 -0500

    Solo borrar, como ya lo habia hecho todo... elimine

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -7,26 +7,12 @@ class RoomsController < ApplicationController
     @room = Room.new
   end
 
-  def edit
-    @room = Room.find(params[:id])
-  end
-
   def create
     @room = Room.new(room_params)
     if @room.save
       redirect_to rooms_path
     else
       render :new
-    end
-  end
-
-  def update
-    @room = Room.find(params[:id])
-
-    if @room.update(room_params)
-      redirect_to rooms_path
-    else
-      render 'edit'
     end
   end
 
