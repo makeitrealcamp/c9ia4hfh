edd086338da6e691a5d1a2d97631e0a0a234ce7b
david medina <davidmeddina@gmail.com>
2019-03-27 13:47:53 -0500

    New validation added

--- app/models/room.rb
+++ app/models/room.rb
@@ -15,6 +15,8 @@
 class Room < ActiveRecord::Base
   validates :title, presence: true
   validates :description, presence: true
+  validates :beds, presence: true
+  validates :guests, presence: true
   validates :description, length: { maximum: 400 }
   validates :beds, numericality: { only_integer: true }
   validates :guests, numericality: { only_integer: true }
