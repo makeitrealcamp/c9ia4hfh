1a7432ba3f22bc768e897951965ab38bb0d8e043
Esteban <estebanmh95@gmail.com>
2019-04-11 18:01:08 -0500

    modelos

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190403010412) do
+ActiveRecord::Schema.define(version: 20190411225940) do
+
+  create_table "albums", force: :cascade do |t|
+    t.string   "name"
+    t.string   "image_url"
+    t.string   "released_at"
+    t.integer  "artist_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.index ["artist_id"], name: "index_albums_on_artist_id"
+  end
+
+  create_table "artists", force: :cascade do |t|
+    t.string   "name"
+    t.string   "image_url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "rooms", force: :cascade do |t|
     t.string   "title"
@@ -21,6 +38,16 @@ ActiveRecord::Schema.define(version: 20190403010412) do
     t.datetime "updated_at",      null: false
     t.string   "image_url"
     t.decimal  "price_per_night"
+  end
+
+  create_table "songs", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "number"
+    t.string   "preview_url"
+    t.integer  "album_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.index ["album_id"], name: "index_songs_on_album_id"
   end
 
 end
