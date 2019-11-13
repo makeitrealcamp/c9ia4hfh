4a2badcb52222c7c055469967adc9692d8436a8a
Dianadhu <dimidusa@gmail.com>
2019-11-13 09:41:26 -0500

    Eliminar

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -17,11 +17,33 @@ class RoomsController < ApplicationController
     end
   end
 
+<<<<<<< Updated upstream
   def destroy
    room = Room.find(params[:id])
    room.destroy
    redirect_to rooms_path
  end
+=======
+  def edit
+      @room = Room.find(params[:id])
+    end
+
+    def update
+      @room = Room.find(params[:id])
+        if @room.update(room_params)
+        redirect_to rooms_path
+        else
+        render :edit
+        end
+    end
+>>>>>>> Stashed changes
+
+    def destroy
+      room = Room.find(params[:id])
+      room.destroy
+      redirect_to rooms_path
+    end
+
 
   protected
     def room_params
